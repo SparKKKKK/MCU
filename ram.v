c41fdbbe99c33be1bba68a1eb054009ebe22ce66
SparKKKKK <547620493@qq.com>
2019-04-02 15:32:30 +0800

    add readme.txt to describe the project

--- ram.v
+++ ram.v
@@ -1,20 +1,30 @@
 module my_ram(
 input wire clk,
 input wire ce_ram,
-
+input wire we_ram,
 input wire [15:0] pc_final,
+input wire [15:0] a,
+input wire [20:0] pc_modify,
 output reg [19:0] ram_dout
     );
 
 reg [19:0] memory [0:255];
 initial $readmemb("ram.bin",memory);
-
+//read 
 always@(posedge clk) begin
     if(ce_ram) begin
-        ram_dout<=#12 memory[pc_final];
+        ram_dout<= #4 memory[pc_final];
     end else begin
-        ram_dout <= #8 15'd0;
+        ram_dout <= #4 15'd0;
     end
+end
+//write 
+always@(posedge clk )begin
+    if(we_ram) begin
+        memory[a] <= #4 pc_modify;
+    end
+    else 
+        memory[a] <= #4 memory[a];
 end
 
 endmodule
---
+++ readme.txt
@@ -0,0 +1,5 @@
+//-------------------------------------------------------------------------
+data:2019/4/1
+describtion:
+estiblish version 1.0,
+include fetch/decoder/execute/reg_file/bus_ctrl/rom/ram/instlist/complier
